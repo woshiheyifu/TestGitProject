f9de98072e4f8193c3237aba5d8e8ec3940e37d3
woshiheyifu <327612253@qq.com>
2016-08-15 23:29:35 +0800

    初始化项目

---
+++ TestGitProject/TestGitProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestGitProject
+//
+//  Created by 何毅夫 on 16/8/15.
+//  Copyright © 2016年 何毅夫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("初始化项目")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
